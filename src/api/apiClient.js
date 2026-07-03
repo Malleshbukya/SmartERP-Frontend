150ecd4ec856b025bed4ee63c46c4a8b53b91798
bukya@Bmalleshnayak <21311A12A3@sreenidhi.edu.in>
2026-07-03 15:35:50 +0530

    Update API URL for production

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smarterp-n6qq.onrender.com/api",
 });
 
 apiClient.interceptors.request.use((config) => {
